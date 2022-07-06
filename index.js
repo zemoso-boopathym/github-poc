0cb75a46e597d91de2b5579c1f65586e6ef65e73
zemoso-boopathym <bhoopathy.muthusaamy@zemosolabs.com>
2022-07-06 13:38:30 +0530

    Implemented Graphql API for github public api POC

---
+++ index.js
@@ -0,0 +1,19 @@
+const { ApolloServer } = require('apollo-server');
+
+const { typeDefs } = require('./schema');
+const { Query } = require('./resolvers/Query');
+
+const resolvers = {
+  Query,
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: 'bounded',
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
